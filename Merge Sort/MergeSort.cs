6b34b2406bc335566a9b2b8eca35e613d82f0da3
NarekXachatryan <50409595+NarekXachatryan@users.noreply.github.com>
2019-05-22 12:46:32 +0400

    Add files via upload

---
+++ Merge Sort/MergeSort.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    public static class MergeSort
+    {
+        public static void Merge(int[] arr)
+        {
+            int leftsize = arr.Length / 2;
+            int rightsize = arr.Length - arr.Length / 2;
+            int[] left = new int[leftsize];
+            int[] right = new int[rightsize];
+            Console.WriteLine();
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+
+                left[i] = arr[i];
+
+            }
+            Console.WriteLine();
+            int k = 0;
+            for (int i = arr.Length / 2; i < arr.Length; i++)
+            {
+
+                right[k] = arr[i];
+                k++;
+            }
+
+            if (leftsize > 1)
+            {
+                Merge(left);
+            }
+
+
+            if (rightsize > 1)
+            {
+                Merge(right);
+            }
+
+            MergeSort(arr, left, right);
+
+        }
+        public static void MergeSort(int[] arr, int[] left, int[] right)
+        {
+            int m = 0;
+            int n = 0;
+            while (m < left.Length && n < right.Length)
+            {
+                if (left[m] > right[n])
+                {
+                    arr[m + n] = right[n];
+                    arr[m + n + 1] = left[m];
+                    n++;
+                }
+                else
+                {
+                    arr[n + m] = left[m];
+                    arr[n + m + 1] = right[n];
+                    m++;
+                }
+            }
+            if (m + n < arr.Length - 1)
+            {
+                if (n == right.Length)
+                {
+                    for (int i = 0; i <= arr.Length - 2 - m - n; i++)
+                    {
+                        arr[m + n + 1 + i] = right[m + 1 + i];
+                    }
+                }
+                else if (m == right.Length)
+                {
+                    for (int i = 0; i <= arr.Length - 2 - m - n; i++)
+                    {
+                        arr[m + n + 1 + i] = right[n + 1 + i];
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Merge Sort/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            Random rand = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] = rand.Next(0, 100));
+                Console.Write("\t");
+            }
+            Console.WriteLine();
+            MergeSort.Merge(arr);
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i]);
+                Console.Write("\t");
+            }
+        }
+    }
+}
+
+
